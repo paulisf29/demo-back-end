4ee793c7c56af1673f89cec00dfd0b4a11a3b8c7
Paulina Fallas <pfallas99@gmail.com>
2023-08-19 12:31:58 -0600

    init

---
+++ src/main/java/com/example/demo/AirplaneModel.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+public class AirplaneModel {
+    String make;
+    int model;
+
+    public AirplaneModel(String make, int model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public int getModel() {
+        return model;
+    }
+
+    public void setModel(int model) {
+        this.model = model;
+    }
+
+    
+
+    
+}
